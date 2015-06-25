9b6204c69ff1f022434b8f2f6ce8db7f0c9d72ae
Trey Huffine <treyhuffine@gmail.com>
2015-06-24 19:45:04 -0700

    Set up basic code and change folder name

--- main.js
+++ main.js
@@ -3,4 +3,31 @@ var fs = require("fs"),
     url = require("url"),
     request = require("request"),
     exec = require("child_process").exec,
-    Firebase = require('firebase');
+    Firebase = require('firebase'),
+    marked = require("marked");
+// http.createServer(responseHandler).listen(process.env.PORT);
+http.createServer(responseHandler).listen(8888);
+
+function responseHandler(req, resp) {
+  if (req.url.match("fav")) {
+    resp.end("");
+    return;
+  }
+
+  // if (req.url === "/") {
+  //   resp.writeHead(200, {"Content-Type": "text/html"});
+  //   fs.readFile('index.html', 'utf8', function (err,data) {
+  //     resp.end(data);
+  //   });
+  // }
+  console.log(req.url.substring(0,10));
+  if (req.url.substring(0,10) === "/markdown/") {
+    resp.writeHead(200, {"Content-Type": "text/html"});
+    fs.readFile('index.html', 'utf8', function (err,data) {
+      resp.end(data);
+    });
+  }
+  else {
+    resp.end("Error. Use /markdown/text to translate a string");
+  }
+}
