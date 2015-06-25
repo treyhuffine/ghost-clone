980333678c36052e3e0fde4d12398b68950570d5
Trey Huffine <treyhuffine@gmail.com>
2015-06-24 17:22:11 -0700

    Add node packages

---
+++ main.js
@@ -0,0 +1,6 @@
+var fs = require("fs"),
+    http = require("http"),
+    url = require("url"),
+    request = require("request"),
+    exec = require("child_process").exec,
+    Firebase = require('firebase');
